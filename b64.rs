20934a06d0f5d7049d9fd8e48abd6781edac01d1
Ayash-Bera <ayashbera@gmail.com>
2025-04-16 20:05:37 +0530

    Update

---
+++ b64.rs
@@ -0,0 +1,51 @@
+//use std::io;
+#[warn(unused_imports)]
+fn main() {
+    let hxd = "4a6f686e";
+
+    println!("Input hex: {}", hxd); // ✅ Step 1: Check if main runs at all
+
+    match hexval(hxd) {
+        Ok(bytes) => {
+            println!("Parsing successful. Bytes:");
+            for byte in bytes {
+                println!("Dec: {}, Bin: {:08b}", byte, byte);
+            }
+        }
+        Err(e) => {
+            println!("Parsing failed with error: {}", e);
+        }
+    }
+}
+
+
+fn chartohex(ch:char)->Result<u8,String>{
+    match ch{
+        '0'..='9'=> Ok(ch as u8 -b'0'),
+        'a'..='f'=> Ok(ch as u8 - b'a' +10),
+        'A'..='F'=> Ok(ch as u8 - b'A' +10),
+        _ =>Err(format!("Err with : {}",ch)),
+    }
+}
+
+
+
+fn hexval(hex: &str)->Result<Vec<u8>,String>{
+    let mut val= Vec::new();  
+    let hexb= hex.as_bytes();
+    if hex.len() %2!=0{
+        return Err("Error".to_string());
+    }
+    else{
+        for chunk in hexb.chunks(2){
+            if chunk.len()!=2{
+                return Err("not long enuf".to_string());
+            }
+            let high = chartohex(chunk[0]as char)?;
+            let low = chartohex(chunk[1]as char)?;
+            let binf= high*16 + low;
+            val.push(binf);
+        }    
+        Ok(val)
+    }
+}
